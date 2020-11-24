74f0553374a3d3ea52498e8e4ed0e7313e510343
damian <dfauth@gmail.com>
2020-11-24 23:11:24 +0800

    initial commit

---
+++ src/main/java/com/github/dfauth/trycatch/TryCatch.java
@@ -0,0 +1,78 @@
+package com.github.dfauth.trycatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class TryCatch {
+
+    private static final Logger logger = LoggerFactory.getLogger(TryCatch.class);
+
+    public static <T> ThrowableHandler<T> propagationHandler() {
+        return t -> {
+            throw new RuntimeException(t);
+        };
+    }
+    private static Runnable noOpFinalRunnable = () -> {};
+
+    public static <T> T tryCatch(ExceptionalSupplier<T> supplier) {
+        return tryCatch(supplier, propagationHandler(), noOpFinalRunnable);
+    }
+
+    public static <T> T tryCatch(ExceptionalSupplier<T> supplier, ThrowableHandler<T> handler) {
+        return tryCatch(supplier, handler, noOpFinalRunnable);
+    }
+
+    public static <T> T tryCatch(ExceptionalSupplier<T> supplier, ThrowableHandler<T> handler, Runnable finalRunnable) {
+        try {
+            return supplier.get();
+        } catch (Throwable t) {
+            logger.error(t.getMessage(), t);
+            return handler.apply(t);
+        } finally {
+            finalRunnable.run();
+        }
+    }
+
+    public static void tryCatch(ExceptionalRunnable runnable) {
+        tryCatch(runnable, t -> {
+            throw new RuntimeException(t);
+        }, noOpFinalRunnable);
+    }
+
+    public static void tryCatchIgnore(ExceptionalRunnable runnable) {
+        tryCatch(runnable, t -> {}, noOpFinalRunnable);
+    }
+
+    public static void tryCatch(ExceptionalRunnable runnable, Consumer<Throwable> handler) {
+        tryCatch(runnable, handler, noOpFinalRunnable);
+    }
+
+    public static void tryCatch(ExceptionalRunnable runnable, Consumer<Throwable> handler, Runnable finalRunnable) {
+        try {
+            runnable.run();
+        } catch (Throwable t) {
+            logger.error(t.getMessage(), t);
+            handler.accept(t);
+        } finally {
+            finalRunnable.run();
+        }
+    }
+
+    @FunctionalInterface
+    interface ExceptionalSupplier<T> {
+        T get() throws Throwable;
+    }
+
+    @FunctionalInterface
+    interface ExceptionalRunnable {
+        void run() throws Throwable;
+    }
+
+    interface ThrowableHandler<T> extends Function<Throwable, T> {};
+}
