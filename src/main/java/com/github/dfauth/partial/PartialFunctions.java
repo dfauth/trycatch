5e0433cf3f351c0ec877377921d1f390860d7e2f
damian <dfauth@gmail.com>
2020-12-13 00:28:49 +0800

    qualify static call

--- src/main/java/com/github/dfauth/partial/PartialFunctions.java
+++ src/main/java/com/github/dfauth/partial/PartialFunctions.java
@@ -6,22 +6,23 @@ import com.github.dfauth.trycatch.Try;
 
 import java.util.function.Function;
 
+import static com.github.dfauth.partial.PartialFunction.fromPredicateAndFunction;
 import static com.github.dfauth.trycatch.TryCatch.tryCatch;
 
 public class PartialFunctions {
 
     public static <T,R extends T> PartialFunction<T,R> downcast(Function<T,R> f) {
-        return PartialFunction.fromPredicateAndFunction(i -> tryCatch(() -> {
+        return fromPredicateAndFunction(i -> tryCatch(() -> {
             f.apply(i);
             return true;
         }, t -> false), f);
     }
 
     public static <T,R extends T> PartialFunction<Try<T>, Success<T>> isSuccessOf(Class<T> classOfT) {
-        return PartialFunction.fromPredicateAndFunction((Try<T> t) -> t.isSuccess(), (Try<T> t) -> t.toSuccess());
+        return fromPredicateAndFunction((Try<T> t) -> t.isSuccess(), (Try<T> t) -> t.toSuccess());
     }
 
     public static <T,R extends T> PartialFunction<Try<T>, Failure<T>> isFailureOf(Class<T> classOfT) {
-        return PartialFunction.fromPredicateAndFunction((Try<T> t) -> t.isFailure(), (Try<T> t) -> t.toFailure());
+        return fromPredicateAndFunction((Try<T> t) -> t.isFailure(), (Try<T> t) -> t.toFailure());
     }
 }
