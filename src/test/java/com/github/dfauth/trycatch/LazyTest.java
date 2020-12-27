315108694841c38254ce4c5402b37a5db1ad480c
Damian Fauth <dfauth@gmail.com>
2020-12-27 08:45:53 +0800

    LazyTest.testMap

--- src/test/java/com/github/dfauth/trycatch/LazyTest.java
+++ src/test/java/com/github/dfauth/trycatch/LazyTest.java
@@ -15,7 +15,7 @@ public class LazyTest {
         };
         Lazy<TestObj> lazy = Lazy.of(() -> {
             ref.cnt++;
-            return new TestObj();
+            return new TestObj(1);
         });
         assertEquals(0, ref.cnt);
         TestObj it = lazy.get();
@@ -25,6 +25,38 @@ public class LazyTest {
         assertEquals(1, ref.cnt);
     }
 
+    @Test
+    public void testMap() {
+        var ref = new Object() {
+            int cnt = 0;
+        };
+        Lazy<TestObj> lazy = Lazy.of(() -> {
+            ref.cnt++;
+            return new TestObj(1);
+        });
+        assertEquals(0, ref.cnt);
+        Lazy<TestObj> lazy2 = lazy.map(t -> new TestObj(t.getI() * 2));
+        assertNotNull(lazy2);
+        assertEquals(0, ref.cnt);
+
+        TestObj it2 = lazy2.get();
+        assertEquals(2, it2.getI());
+        assertEquals(1, ref.cnt);
+
+        TestObj it = lazy.get();
+        assertEquals(1, it.getI());
+        assertEquals(1, ref.cnt);
+    }
+
     class TestObj {
+        private final int i;
+
+        TestObj(int i) {
+            this.i = i;
+        }
+
+        public int getI() {
+            return i;
+        }
     }
 }
